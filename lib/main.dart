04aa68a8081c9cc216f3f2d6bb9c7a472ddc62db
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-11-26 07:31:34 -0600

    Project has been updated

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello'),
+      ),
+      color: Colors.red,
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
