cee5ce216d2787a1d14cfab4b80381a3ffa1d517
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-11-27 04:49:00 -0600

    Project has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello'),
-      ),
-      color: Colors.red,
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'),
+          ),
+        ),
+        backgroundColor: Colors.deepPurple[100],
+      ),
     ),
   );
 }
